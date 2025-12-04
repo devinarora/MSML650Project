9b2236a741256efd6bc7885af56ec3827a7727da
devinarora <46658542+devinarora@users.noreply.github.com>
2025-12-04 15:02:58 -0500

    fix config file pointing to local

--- js_client/config.js
+++ js_client/config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG = {
   userPoolId: "us-east-2_gBq4DlIOc",
   userPoolClientId: "1usfarvlqk1filbgeqskn2ruul",
   cognitoDomain: "us-east-2gbq4dlioc.auth.us-east-2.amazoncognito.com", // e.g., "myapp.auth.us-east-1.amazoncognito.com"
-  redirectUri: "http://localhost:4200/",
+  redirectUri: "https://dpz1lhckwu42w.cloudfront.net/", // change to "http://localhost:4200/" only when testing
   apiBaseUrl: "https://f58yls4u54.execute-api.us-east-2.amazonaws.com/dev/backend", // no trailing slash
   mediaBucketPublicBaseUrl: "https://YOUR_CLOUDFRONT_DOMAIN/media/", // if you front S3 media with CloudFront; optional
 };
